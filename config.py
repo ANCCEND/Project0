cb765be561bb9e0de772df797f3b9a29bf98cfc8
Anccend <3064429095@qq.com>
2025-02-14 13:26:59 +0000

    config.py改动

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
 
     # JWT 配置
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "your_jwt_secret_key")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(minutes=int(os.getenv("JWT_ACCESS_TOKEN_EXPIRES")))  # 默认 15 分钟
     JWT_REFRESH_TOKEN_EXPIRES = timedelta(days=int(os.getenv("JWT_REFRESH_TOKEN_EXPIRES")))  # 默认 30 天
